18798de1e500a406d729fb0cee02b76810870fb6
Hairi <hairi.gulamsarwar47@gmail.com>
2024-09-09 03:34:05 +0800

    Initial Commit

---
+++ ElkaElzaFitness/ElkaElzaFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElkaElzaFitnessApp.swift
+//  ElkaElzaFitness
+//
+//  Created by Hairi Dev on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ElkaElzaFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
